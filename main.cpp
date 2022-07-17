4aac54b146ff5d0e7eed0dc9e5cb129819309c53
Archie Collison <archie.collison@gmail.com>
2022-07-17 19:28:36 +1000

    Basic framework

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 
+class Calculator
+{
+public:
+    double i, j;
+    int add()
+    {
+        return i+j;
+    }
+    int subtract()
+    {
+        return i-j;
+    }
+    int multiply()
+    {
+        return i*j;
+    }
+    int divide()
+    {
+        return i/j;
+    }
+};
+
+
 int main() {
     std::cout << "hello world!" << std::endl;
-    return 0;
+    Calculator myCalc;
+    myCalc.i = 4;
+    myCalc.j = 3;
+    std::cout << myCalc.add() << std::endl;
+    std::cout << myCalc.subtract() << std::endl;
+    std::cout << myCalc.multiply() << std::endl;
+    std::cout << myCalc.divide() << std::endl;
 }
